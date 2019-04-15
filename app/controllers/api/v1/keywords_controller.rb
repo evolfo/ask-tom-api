a65856018a7d4777b2365cd7bd59bb0e8d2621cb
Angelo Spampinato <spampinatodesign@gmail.com>
2019-04-15 14:17:00 -0400

    PLZ WORK PLZ

--- app/controllers/api/v1/keywords_controller.rb
+++ app/controllers/api/v1/keywords_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::KeywordsController < ApplicationController
 before_action :find_keyword, only: [:show, :update]
 
-skip_before_action :authorized, only: [:create, :index, :show]
+skip_before_action :authorized, only: [:create, :index, :show, :update]
 
   def index
   	@keywords = Keyword.all
