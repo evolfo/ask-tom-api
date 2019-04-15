be73b542a5275ffcadca214705e26c28234f0fec
Angelo Spampinato <spampinatodesign@gmail.com>
2019-04-15 12:00:14 -0400

    fixing routes file

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: [:create]
-      post '/login'
+      # post '/login', to: 'auth#create'
       resources :tools, only: [:index, :create, :show]
       resources :ideas, only: [:index, :create, :show]
       resources :keywords, only: [:index, :create, :show]
