26318152755f2b04a8a64e368bed5920a83aa141
Angelo Spampinato <spampinatodesign@gmail.com>
2019-04-15 19:32:06 -0400

    plz work

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -4,17 +4,21 @@ class Api::V1::AuthController < ApplicationController
 	skip_before_action :authorized, only: [:create]
 
 	def show
-	    @token = decoded_token
-	    @user_id = @token[0]['user_id']
-	    @user = User.find(@user_id)
-	    render json: { user: @user }
+	    # @token = decoded_token
+	    # @user_id = @token[0]['user_id']
+	    # @user = User.find(@user_id)
+	    # render json: { user: @user }
+	     jwt = request.headers['Authorization']
+	    id = JWT.decode(jwt, "tom_idea_generator")[0]["user_id"]
+	    @user = User.find(id)
+	    render json: {user: @user}
   	end
 
 	def create
 	  @user = User.find_by(name: user_login_params[:name])
 
 	  if @user && @user.authenticate(user_login_params[:name])
-	  	token = encode_token({ user_id: @user.id })
+	  	token = encode_token({ user_id: @user.id }, "tom_idea_generator")
 	  	render json: { user: UserSerializer.new(@user), jwt: token }, status: :accepted
 	  else
 	  	render json: { message: 'Invalid username or password' }, status: :anauthorized
