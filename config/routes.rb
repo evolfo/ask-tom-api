e563bd5a839552bc2bca366acec7723d1c86e4dc
Angelo Spampinato <spampinatodesign@gmail.com>
2019-04-15 12:04:55 -0400

    adding index route for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :users, only: [:create]
-      # post '/login', to: 'auth#create'
+      resources :users, only: [:index, :create]
+      post '/login', to: 'auth#create'
       resources :tools, only: [:index, :create, :show]
       resources :ideas, only: [:index, :create, :show]
       resources :keywords, only: [:index, :create, :show]
