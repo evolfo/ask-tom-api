b35ba90ba85df255b2b92386cff29652d56f6bc7
Angelo Spampinato <spampinatodesign@gmail.com>
2019-04-15 16:00:19 -0400

    USERNAME USERNAMEUSERNAMEUSERNAMEUSERNAMEUSERNAMEUSERNAMEUSERNAMEUSERNAMEUSERNAMEUSERNAMEUSERNAMEUSERNAMEUSERNAMEUSERNAMEUSERNAMEUSERNAMEUSERNAMEUSERNAMEUSERNAMEUSERNAMEUSERNAMEUSERNAMEUSERNAMEUSERNAMEUSERNAMEUSERNAMEUSERNAMEUSERNAMEUSERNAMEUSERNAMEUSERNAMEUSERNAMEUSERNAMEUSERNAMEUSERNAMEUSERNAMEUSERNAMEUSERNAMEUSERNAMEUSERNAMEUSERNAMEUSERNAMEUSERNAMEUSERNAMEUSERNAMEUSERNAMEUSERNAMEUSERNAMEUSERNAMEUSERNAMEUSERNAMEUSERNAMEUSERNAMEUSERNAMEUSERNAMEUSERNAMEUSERNAMEUSERNAMEUSERNAMEUSERNAMEUSERNAMEUSERNAMEUSERNAMEUSERNAMEUSERNAMEUSERNAMEUSERNAMEUSERNAMEUSERNAMEUSERNAMEUSERNAMEUSERNAMEUSERNAMEv

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -4,9 +4,9 @@ class Api::V1::AuthController < ApplicationController
 	skip_before_action :authorized, only: [:create]
 
 	def create
-	  @user = User.find_by(username: user_login_params[:username])
+	  @user = User.find_by(name: user_login_params[:name])
 
-	  if @user && @user.authenticate(user_login_params[:username])
+	  if @user && @user.authenticate(user_login_params[:name])
 	  	token = encode_token({ user_id: @user.id })
 	  	render json: { user: UserSerializer.new(@user), jwt: token }, status: :accepted
 	  else
