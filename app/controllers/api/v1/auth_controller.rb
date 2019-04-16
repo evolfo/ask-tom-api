1cac35253181c913bbc598cd14477f2599f0fa31
Angelo Spampinato <spampinatodesign@gmail.com>
2019-04-15 21:17:20 -0400

    plz work

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -17,7 +17,7 @@ class Api::V1::AuthController < ApplicationController
 	def create
 	  @user = User.find_by(name: user_login_params[:name])
 	  if @user && @user.authenticate(user_login_params[:password])
-	  	token = encode_token({ user_id: @user.id }, "tom_idea_generator")
+	  	token = encode_token({ user_id: @user.id })
 	  	render json: { user: @user.name, jwt: token }, status: :accepted
 	  else
 	  	render json: { message: 'Invalid username or password' }, status: :unauthorized
