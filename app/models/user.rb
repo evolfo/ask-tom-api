1bbe4c6446aeec9af7babd9c96b840bda60f29de
Angelo Spampinato <spampinatodesign@gmail.com>
2019-04-16 15:14:50 -0400

    fixing

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,6 @@ class User < ApplicationRecord
 	has_many :user_ideas
 	has_many :ideas, through: :user_ideas
 
-	validates :name, obscenity: true
-
 	validates :name, presence: true, uniqueness: { case_sensitive: false }
 	validates :name, format: { with: /\A[a-zA-Z0-9\s]+\z/i, message: "can only contain letters and numbers." }
 	validates_length_of :name, maximum: 12
