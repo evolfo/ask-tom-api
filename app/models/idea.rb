091b1b1e707c23e0e8014d5595ee2753b3ddd06e
Angelo Spampinato <spampinatodesign@gmail.com>
2019-04-16 17:36:52 -0400

    uniqueness for idea_type

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -3,5 +3,7 @@ class Idea < ApplicationRecord
 	has_many :users, through: :user_ideas
 
 	validates :idea_type, presence: true
+	validates :idea_type, uniqueness: true
+	
 	validates :mod, presence: true
 end
