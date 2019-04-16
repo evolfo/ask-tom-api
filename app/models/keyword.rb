83167fcd11b54cd91247ffc34afc185cd5a9bf70
Angelo Spampinato <spampinatodesign@gmail.com>
2019-04-16 14:49:32 -0400

    removing obscenity

--- app/models/keyword.rb
+++ app/models/keyword.rb
@@ -1,14 +1,14 @@
 class Keyword < ApplicationRecord
 	validates :subject, format: { with: /\A[a-zA-Z\s]+\z/i, message: "can only contain letters." }
 	validates_length_of :subject, maximum: 15
-	validates :subject, obscenity: true
+	# validates :subject, obscenity: true
 
 	validates :keyword_type, format: { with: /\A[a-zA-Z\s]+\z/i, message: "can only contain letters." }
 	validates_length_of :keyword_type, maximum: 15
-	validates :keyword_type, obscenity: true
+	# validates :keyword_type, obscenity: true
 
 	validates :purpose, format: { with: /\A[a-zA-Z\s]+\z/i, message: "can only contain letters." }
 	validates_length_of :purpose, maximum: 15
-	validates :purpose, obscenity: true
+	# validates :purpose, obscenity: true
 end
 
