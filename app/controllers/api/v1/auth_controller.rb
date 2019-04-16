1c296bb4942d173f3df20db18f8789ee5f727462
Angelo Spampinato <spampinatodesign@gmail.com>
2019-04-15 21:14:41 -0400

    plz work

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -8,28 +8,21 @@ class Api::V1::AuthController < ApplicationController
 	    @user_id = @token[0]['user_id']
 	    @user = User.find(@user_id)
 	    render json: { user: @user }
+	    # jwt = request.headers['Authorization']
+	    # id = JWT.decode(jwt, "tom_idea_generator")[0]["user_id"]
+	    # @user = User.find(id)
+	    # render json: {user: @user}
   	end
 
 	def create
 	  @user = User.find_by(name: user_login_params[:name])
 	  if @user && @user.authenticate(user_login_params[:password])
-	  	@token = encode_token({ user: @user }, "tom_idea_generator")
+	  	token = encode_token({ user_id: @user.id }, "tom_idea_generator")
 	  	render json: { user: @user.name, jwt: token }, status: :accepted
 	  else
 	  	render json: { message: 'Invalid username or password' }, status: :unauthorized
 	  end
 	end
-
-	# def create
- #    @user = User.find_by(username: login_params[:username])
- #    if @user && @user.authenticate(login_params[:password])
- #      @token = encode_token({ user_id: @user.id})
- #      render json: { username: @user.username, jwt: @token }, status: :accepted
- #    else
- #      render json: {
- #        errors: 'your username or password are incorrect'
- #      }, status: :unauthorized
- #    end
 
 	private
 
