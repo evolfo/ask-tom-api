e2316d095e7ff331ef5fc0591897e3a175439308
Angelo Spampinato <spampinatodesign@gmail.com>
2019-04-16 18:09:25 -0400

    fixing seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.destroy_all
 Tool.destroy_all
 Keyword.destroy_all
-UserIdea.destroy_all
 
 jelly = User.create(name: "jelly")
 tracy = User.create(name: "jelly2")
